8d153f01076e2346e114a97ff542a5cbfe095f25
Derek Garnett <drkgrntt@gmail.com>
2023-02-08 19:52:20 -0600

    fix query

--- controllers/shows.go
+++ controllers/shows.go
@@ -18,7 +18,7 @@ func NewShowsController(DB *gorm.DB) ShowsController {
 
 func (c *ShowsController) GetProductions(ctx *gin.Context) {
 	var productions []models.Production
-	c.DB.Find(&productions).Where("has_tkts_data = true").Order("last_scanned_at DESC").Order("last_shown_at DESC")
+	c.DB.Find(&productions).Where("has_tkts_data = ?", true).Order("last_scanned_at DESC").Order("last_shown_at DESC")
 
 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": gin.H{"productions": productions}})
 }
